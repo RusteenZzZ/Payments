b88286d7d04af51ccd9210bafa03ebaa6be5edab
RusteenZzZ <aggreex@gmail.com>
2021-08-30 14:05:21 +0400

    feat: created DTO for user and product

---
+++ src/main/java/SuperDuperMegaProject/Rustam/DTO/Product.java
@@ -0,0 +1,8 @@
+package SuperDuperMegaProject.Rustam.DTO;
+
+public class Product {
+    private Long id;
+    private String productName;
+    private Double price;
+    private Integer amount;
+}
---
+++ src/main/java/SuperDuperMegaProject/Rustam/DTO/User.java
@@ -0,0 +1,9 @@
+package SuperDuperMegaProject.Rustam.DTO;
+
+public class User {
+    private Long id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private Double balance;
+}
