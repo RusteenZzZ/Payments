b489b0cf842c25ba416bfa83505e582cf85f9d04
RusteenZzZ <aggreex@gmail.com>
2021-08-31 15:56:55 +0400

    fix: updated the return type of feigns

--- src/main/java/SuperDuperMegaProject/Rustam/FeignClient/ProductFeign.java
+++ src/main/java/SuperDuperMegaProject/Rustam/FeignClient/ProductFeign.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Optional;
+
 @FeignClient(name = "Product", url = "localhost:8000/")
 public interface ProductFeign {
 
     @RequestMapping(method = RequestMethod.GET, value = "/product/{id}")
-    Product getProduct(@PathVariable("id") Long id);
+    Optional<Product> getProduct(@PathVariable("id") Long id);
 }
--- src/main/java/SuperDuperMegaProject/Rustam/FeignClient/UserFeign.java
+++ src/main/java/SuperDuperMegaProject/Rustam/FeignClient/UserFeign.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Optional;
+
 @FeignClient(name = "User", url = "localhost:8000/")
 public interface UserFeign {
 
     @RequestMapping(method = RequestMethod.GET, value = "/user/{id}")
-    User getUser(@PathVariable("id") Long id);
+    Optional<User> getUser(@PathVariable("id") Long id);
 }
