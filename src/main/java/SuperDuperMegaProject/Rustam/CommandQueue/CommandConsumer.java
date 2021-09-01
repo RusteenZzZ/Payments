7f5c1e58278f6911dfd50e29b4f132c80984373b
RusteenZzZ <aggreex@gmail.com>
2021-09-01 10:57:37 +0400

    feat: added creating of transaction records in the consumer of commands

--- src/main/java/SuperDuperMegaProject/Rustam/CommandQueue/CommandConsumer.java
+++ src/main/java/SuperDuperMegaProject/Rustam/CommandQueue/CommandConsumer.java
@@ -2,19 +2,23 @@ package SuperDuperMegaProject.Rustam.CommandQueue;
 
 import SuperDuperMegaProject.Rustam.DTO.Product;
 import SuperDuperMegaProject.Rustam.DTO.User;
+import SuperDuperMegaProject.Rustam.Entity.Transaction;
 import SuperDuperMegaProject.Rustam.FeignClient.ProductFeign;
 import SuperDuperMegaProject.Rustam.FeignClient.UserFeign;
 import SuperDuperMegaProject.Rustam.ReplyQueue.ReplyProducer;
 import SuperDuperMegaProject.Rustam.ReplyQueue.ReplyType;
+import SuperDuperMegaProject.Rustam.Service.TransactionService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @Component
 public class CommandConsumer {
 
+    private final TransactionService transactionService;
     private final UserFeign userFeign;
     private final ProductFeign productFeign;
     private final ReplyProducer replyProducer;
@@ -29,11 +33,13 @@ public class CommandConsumer {
     private List<Integer> amountOfProducts;
 
     public CommandConsumer(
+            TransactionService transactionService,
             UserFeign userFeign,
             ProductFeign productFeign,
             ReplyProducer replyProducer,
             ReplyType replyType
     ){
+        this.transactionService = transactionService;
         this.userFeign = userFeign;
         this.productFeign = productFeign;
         this.replyProducer = replyProducer;
@@ -49,12 +55,14 @@ public class CommandConsumer {
         this.user = this.userFeign.getUser(this.userId);
 
         // ERROR: User doesn't exist
+        this.createTransactionRecords(this.replyType.ERROR);
         if(user.isEmpty()){
             this.replyProducer.reply(this.replyType.ERROR);
             return;
         }
 
         // ERROR: Not matching numbers of products and amounts
+        this.createTransactionRecords(this.replyType.ERROR);
         if(this.productIds.size() != this.amountOfProducts.size()){
             this.replyProducer.reply(this.replyType.ERROR);
             return;
@@ -68,12 +76,14 @@ public class CommandConsumer {
             product = this.productFeign.getProduct(productIds.get(i));
 
             // ERROR: Certain product doesn't exist
+            this.createTransactionRecords(this.replyType.ERROR);
             if(product.isEmpty()){
                 this.replyProducer.reply(this.replyType.ERROR);
                 return;
             }
 
             // FAIL: Not sufficient amount of products on the storage
+            this.createTransactionRecords(this.replyType.FAIL);
             if(this.amountOfProducts.get(i) > product.get().amount){
                 this.replyProducer.reply(this.replyType.FAIL);
                 return;
@@ -83,12 +93,33 @@ public class CommandConsumer {
         }
 
         // FAIL: Not sufficient balance of user
+        this.createTransactionRecords(this.replyType.FAIL);
         if(this.user.get().balance < price){
             this.replyProducer.reply(this.replyType.FAIL);
             return;
         }
 
         // SUCCESS: Everything is fine
+        this.createTransactionRecords(this.replyType.SUCCESS);
         this.replyProducer.reply(this.replyType.SUCCESS);
+    }
+
+    private void createTransactionRecords(String respond){
+        Optional<Product> product;
+
+        for(int i = 0; i < this.productIds.size(); i++){
+            product = this.productFeign.getProduct(productIds.get(i));
+
+            this.transactionService.addTransaction(
+                    new Transaction(
+                            this.userId,
+                            new Date(),
+                            this.productIds.get(i),
+                            this.amountOfProducts.get(i),
+                            product.get().price * this.amountOfProducts.get(i),
+                            this.replyType.SUCCESS
+                    )
+            );
+        }
     }
 }
