d9b87e95d4d5bb241253e6d51fc564f5223c2e73
RusteenZzZ <aggreex@gmail.com>
2021-09-02 11:28:16 +0400

    feat: added new transaction creations for ERROR and FAIL cases

--- src/main/java/SuperDuperMegaProject/Rustam/Consumer/CommandConsumer.java
+++ src/main/java/SuperDuperMegaProject/Rustam/Consumer/CommandConsumer.java
@@ -57,14 +57,14 @@ public class CommandConsumer {
         this.user = this.userFeign.getUser(this.userId);
 
         // ERROR: User doesn't exist
-        this.createTransactionRecords(this.replyType.ERROR);
+        this._createErrorTransactionRecords();
         if(user.isEmpty()){
             this.replyProducer.reply(this.replyType.ERROR);
             return;
         }
 
         // ERROR: Not matching numbers of products and amounts
-        this.createTransactionRecords(this.replyType.ERROR);
+        this._createErrorTransactionRecords();
         if(this.productIds.size() != this.amountOfProducts.size()){
             this.replyProducer.reply(this.replyType.ERROR);
             return;
@@ -78,14 +78,14 @@ public class CommandConsumer {
             product = this.productFeign.getProduct(productIds.get(i));
 
             // ERROR: Certain product doesn't exist
-            this.createTransactionRecords(this.replyType.ERROR);
+            this._createErrorTransactionRecords();
             if(product.isEmpty()){
                 this.replyProducer.reply(this.replyType.ERROR);
                 return;
             }
 
             // FAIL: Not sufficient amount of products on the storage
-            this.createTransactionRecords(this.replyType.FAIL);
+            this._createFailTransactionRecords();
             if(this.amountOfProducts.get(i) > product.get().amount){
                 this.replyProducer.reply(this.replyType.FAIL);
                 return;
@@ -95,18 +95,52 @@ public class CommandConsumer {
         }
 
         // FAIL: Not sufficient balance of user
-        this.createTransactionRecords(this.replyType.FAIL);
+        this._createFailTransactionRecords();
         if(this.user.get().balance < price){
             this.replyProducer.reply(this.replyType.FAIL);
             return;
         }
 
         // SUCCESS: Everything is fine
-        this.createTransactionRecords(this.replyType.SUCCESS);
+        this._createSuccessTransactionRecords();
         this.replyProducer.reply(this.replyType.SUCCESS);
     }
 
-    private void createTransactionRecords(String respond){
+    private void _createErrorTransactionRecords(){
+        for (Long productId : this.productIds) {
+            this.transactionService.addTransaction(
+                    new Transaction(
+                            this.userId,
+                            new Date(),
+                            productId,
+                            null,
+                            null,
+                            this.replyType.ERROR
+                    )
+            );
+        }
+    }
+
+    private void _createFailTransactionRecords(){
+        Optional<Product> product;
+
+        for(int i = 0; i < this.productIds.size(); i++){
+            product = this.productFeign.getProduct(productIds.get(i));
+
+            this.transactionService.addTransaction(
+                    new Transaction(
+                            this.userId,
+                            new Date(),
+                            this.productIds.get(i),
+                            this.amountOfProducts.get(i),
+                            product.get().price * this.amountOfProducts.get(i),
+                            this.replyType.FAIL
+                    )
+            );
+        }
+    }
+
+    private void _createSuccessTransactionRecords(){
         Optional<Product> product;
 
         for(int i = 0; i < this.productIds.size(); i++){
