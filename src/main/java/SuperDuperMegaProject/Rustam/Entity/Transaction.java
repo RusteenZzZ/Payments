61d7f8688fb2e371a55f5538cb5d3c1da0e96299
RusteenZzZ <aggreex@gmail.com>
2021-09-02 11:16:00 +0400

    fix: cleaned code wia lombok

--- src/main/java/SuperDuperMegaProject/Rustam/Entity/Transaction.java
+++ src/main/java/SuperDuperMegaProject/Rustam/Entity/Transaction.java
@@ -1,11 +1,17 @@
 package SuperDuperMegaProject.Rustam.Entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Transaction {
     @Id
     @SequenceGenerator(
@@ -56,9 +62,6 @@ public class Transaction {
     )
     private String respond;
 
-    public Transaction() {
-    }
-
     public Transaction(
             Long userId,
             Date date,
@@ -73,66 +76,5 @@ public class Transaction {
         this.amount = amount;
         this.price = price;
         this.respond = respond;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public Long getProductId() {
-        return productId;
-    }
-
-    public void setProductId(Long productId) {
-        this.productId = productId;
-    }
-
-    public Integer getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Integer amount) {
-        this.amount = amount;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public String getRespond() {
-        return respond;
-    }
-
-    public void setRespond(String respond) {
-        this.respond = respond;
-    }
-
-    @Override
-    public String toString() {
-        return "Transaction{" +
-                "id=" + id +
-                ", userId=" + userId +
-                ", date=" + date +
-                ", productId=" + productId +
-                ", amount=" + amount +
-                ", price=" + price +
-                ", respond='" + respond + '\'' +
-                '}';
     }
 }
